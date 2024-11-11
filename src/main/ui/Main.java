b831710e4e27785f710da2adc0464c3bc54aa609
harpreeto6 <hs3732065@gmail.com>
2024-11-11 15:04:15 -0800

    Created MainWindow class for GUI app and provided specification for class and it's methods. Inserted stubs inside specified methods

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,11 +1,24 @@
 package ui;
 
+import javax.swing.SwingUtilities;
 
 /*
- * class to initiate HealthApp
+ * class to initiate HealthApp or MainWindow
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        new HealthApp();
+
+
+       // new HealthApp();
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                MainWindow main = new MainWindow();
+                main.show();
+            }
+            
+        });
     }
 }
---
+++ src/main/ui/MainWindow.java
@@ -0,0 +1,46 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/*
+ * This class will act a GUI for the app
+ * Class will be using JSwing library to create Graphical user Interface for our app
+ */
+public class MainWindow {
+
+
+    private JFrame frame;
+    private JPanel buttonPanel;
+
+    public MainWindow() {
+        initialize();
+    }
+
+    //MODIFIES : this
+    //EFFECTS : Initialize the fields and call appropriate methods in appropriate sequence to make the GUI run
+    private void initialize() {
+        //stub
+
+    }
+
+    //MODIFIES : this
+    //EFFECTS : Make the GUI visible
+    public void show() {
+        //stub
+    }
+
+    //MODIFIES : this
+    //EFFECTS : create 2 buttons ("Add Meal" and " View \nProgress" ) and add them to buttonsPanel
+    private void addButtonsToButtonPanel() {
+
+        //stub
+
+    }
+}
